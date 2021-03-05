14086a4b2ec9e2b22e7088abffd9bed1727da889
m.azizkhani <13740mohammad@gmail.com>
2021-03-05 18:24:01 +0330

    Complete Domain

---
+++ src/Domain/RepositoryAbstractions/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entity;
+
+namespace Domain.RepositoryAbstractions
+{
+    public interface IUserRepository
+    {
+        Task CreateUser(User user);
+        Task<bool> IsUserAuthenticated(User user);
+    }
+}
